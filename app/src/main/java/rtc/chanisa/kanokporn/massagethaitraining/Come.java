d9843ecf2d5620f271daa635d4f15acca25bb8c9
masterUNG <phrombutr@gmail.com>
2017-01-07 01:35:46 +0700

    Click Button7

--- app/src/main/java/rtc/chanisa/kanokporn/massagethaitraining/Come.java
+++ app/src/main/java/rtc/chanisa/kanokporn/massagethaitraining/Come.java
@@ -1,22 +1,26 @@
 package rtc.chanisa.kanokporn.massagethaitraining;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
-public class Come extends AppCompatActivity {
+public class Come extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.come);
+
+    }   // Main Method
+
+    public void clickButton7(View view) {
+        startActivity(new Intent(Come.this, MainHub.class));
+        Log.d("7jan", "Click button7");
     }
 
-    public void clickNext(View view) {
-        startActivity(new Intent(Come.this,MainHub.class));
-        finish();
 
-    }
 
-}
+}   // Main Class
